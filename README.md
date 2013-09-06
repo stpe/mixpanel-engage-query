0b21034bc700d3db3d1a8f76c614cc3a7dba2701
Stefan Pettersson <stefan@plan3.se>
2013-09-06 15:00:58 +0200

    First commit. Working version (at least for me…)

---
+++ README.md
@@ -0,0 +1,63 @@
+Command-line tool to query the MixPanel Engage API (https://mixpanel.com/docs/api-documentation/data-export-api#engage-default) for People Data.
+
+This was quickly put together for use of my own. But feel free to fork, submit pull requests or use if you find it useful.
+
+## Installation
+
+Clone repo, make sure you've got Node.js installed, run ``npm install`` in the directory. That's it!
+
+## Example
+
+#### Get help
+
+```
+$ node engage.js
+Usage: node ./engage.js -k [string] -s [string]
+
+Options:
+  -k, --key         MixPanel API key                                                  [required]
+  -s, --secret      MixPanel API secret                                               [required]
+  -q, --query       A segmentation expression
+  -f, --format      Output format, json or csv                                        [default: "json"]
+  -p, --properties  Properties to output. Outputs all properties if none specified.
+
+Missing required arguments: k, s
+```
+
+#### Get everything
+
+``node engage.js -k MIXPANEL_API_KEY -s MIXPANEL_API_SECRET``
+
+#### Only output specific fields
+
+``node engage.js -k MIXPANEL_API_KEY -s MIXPANEL_API_SECRET -p '$email $first_name'``
+
+Example output:
+```
+{ '$email': 'jocke@bigcompany.se', '$first_name': 'Joakim' }
+{ '$email': 'henrik@gmail.com', '$first_name': 'Henrik' }
+{ '$email': 'theguy@gmail.com', '$first_name': 'Jonas' }
+```
+
+Note: one JSON entry per row.
+
+#### Output as CVS instead of JSON
+
+``node engage.js -k MIXPANEL_API_KEY -s MIXPANEL_API_SECRET -f 'csv'``
+
+Example output:
+```
+jocke@bigcompany.se;Joakim
+henrik@gmail.com;Henrik
+theguy@gmail.com;Jonas
+```
+
+Note: currently no special escaping or similar is implemented, so depending on values csv may end up invalid.
+
+#### Query using expression
+
+This example returns people with $last_seen timestamp greater (later) than 29th of August. See MixPanel documentation for segmentation expressions:
+https://mixpanel.com/docs/api-documentation/data-export-api#segmentation-expressions
+
+``node engage.js -k MIXPANEL_API_KEY -s MIXPANEL_API_SECRET -q 'properties["$last_seen"] > "2013-08-29T23:00:00"'``
+
---
+++ engage.js
@@ -0,0 +1,138 @@
+/*jshint node: true */
+"use strict";
+
+var needle      = require('needle'),
+    crypto      = require('crypto'),
+
+    // mixpanel
+    base_url    = "http://mixpanel.com/api/2.0/",
+
+    // according to mixpanel doc https://mixpanel.com/docs/api-documentation/data-export-api#engage-default
+    // response should return page_size, but it doesn't, so we assume it is 1000 for now
+    page_size = 1000;
+
+// options
+var argv = require('optimist')
+    .usage('Usage: $0 -k [string] -s [string]')
+    .demand(['k','s'])
+    .options('k', {
+        alias: 'key',
+        describe: 'MixPanel API key'
+    })
+    .options('s', {
+        alias: 'secret',
+        describe: 'MixPanel API secret'
+    })
+    .options('q', {
+        alias: 'query',
+        // https://mixpanel.com/docs/api-documentation/data-export-api#segmentation-expressions
+        // example: 'properties["$last_seen"] > "2013-08-29T23:00:00"'
+        describe: 'A segmentation expression (see MixPanel API doc)'
+    })
+    .options('f', {
+        alias: 'format',
+        default: 'json',
+        describe: 'Output format, json or csv'
+    })
+    .options('p', {
+        alias: 'properties',
+        describe: "Properties to output (e.g. '$email $first_name'). Outputs all properties if none specified."
+    })
+    .argv;
+
+// get properties to output
+var properties = typeof argv.properties == "string" ? argv.properties.split(" ") : [];
+
+// do the stuff!
+queryEngageApi({
+    page: 0,
+    where: argv.query || ""
+});
+
+// ------------------------------------------
+
+function queryEngageApi(params) {
+    var url = getUrl("engage", params);
+
+    needle.get(url, {}, function(err, resp, data) {
+        // request error
+        if (err) {
+            console.log("Error: " + err);
+            return;
+        }
+
+        // MixPanel API error
+        if (data.error) {
+            console.log('MixPanel API error: ' + data.error);
+            return;
+        }
+
+        processResults(data);
+
+        // unless fewer results than page_size, keep querying for additional pages
+        if (data.results.length >= page_size) {
+            // get next page
+            params.page++;
+            // use session id in next query to speed up api response
+            params.session_id = data.session_id;
+
+            queryEngageApi(params);
+        }
+    });
+}
+
+function processResults(data) {
+    var i, csv, entry, len = data.results.length;
+    for (i = 0; i < len; i++) {
+        entry = {};
+        if (properties.length === 0) {
+            // output all
+            entry = data.results[i].$properties;
+        } else {
+            // only include given properties
+            properties.forEach(function(p) {
+                if (data.results[i].$properties[p]) {
+                    entry[p] = data.results[i].$properties[p];
+                } else {
+                    entry[p] = '';
+                }
+            });
+        }
+
+        // output object unless empty
+        if (Object.keys(entry).length) {
+            if (argv.format == "csv") {
+                csv = [];
+                Object.keys(entry).forEach(function(k) {
+                    csv.push(entry[k]);
+                });
+                console.log(csv.join(";"));
+            } else {
+                console.log(entry);
+            }
+        }
+    }
+}
+
+function getUrl(endpoint, args) {
+    // add api_key and 60 sec expire
+    args.api_key = argv.key;
+    args.expire = Math.round(Date.now() / 1000) + 60;
+
+    // see https://mixpanel.com/docs/api-documentation/data-export-api#auth-implementation
+    var arg_keys = Object.keys(args),
+        sorted_keys = arg_keys.sort(),
+        concat_keys = "",
+        params = [];
+
+    for (var i = 0; i < sorted_keys.length; i++) {
+        params.push(sorted_keys[i] + "=" + args[sorted_keys[i]]);
+        concat_keys += params[params.length - 1];
+    }
+
+    // sign
+    var sig = crypto.createHash('md5').update(concat_keys + argv.secret).digest("hex");
+
+    // return request url
+    return base_url + endpoint + "/?" + params.join("&") + "&sig=" + sig;
+}
