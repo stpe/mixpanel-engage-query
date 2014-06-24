a3b18a137e17ecdc2c674d996f94d097d8a78183
Stefan Pettersson <stefpet@gmail.com>
2014-06-24 11:43:34 +0200

    Keep json output on one line for easy digestion

--- engage.js
+++ engage.js
@@ -129,7 +129,7 @@ function processResults(data) {
             console.log(csv.join(";"));
         } else {
             // json
-            console.log(entry);
+            console.log(JSON.stringify(entry));
         }
     }
 }
